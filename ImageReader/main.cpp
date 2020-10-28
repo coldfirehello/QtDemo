fe1e2f872478208e9ef94686a53ccdf2febac66c
guanxinnan@aixuexi.com <guanxinnan@aixuexi.com>
2020-10-28 20:13:51 +0800

    上传ImageReader示例代码

---
+++ ImageReader/main.cpp
@@ -0,0 +1,51 @@
+﻿#include "widget.h"
+
+#include <QApplication>
+#include <QLabel>
+#include <QMovie>
+#include <QBitmap>
+#include <QPainter>
+#include "roundedapnganimationlabel.h"
+#include <QWidget>
+#include <QVBoxLayout>
+
+int main(int argc, char *argv[])
+{
+    QApplication a(argc, argv);
+
+    QWidget w;
+    w.resize(292, 254);
+    QVBoxLayout layout;
+
+    QMovie movie("D:/work/axxstudent/Bin/Environment/images/groupRoom/openCurtainAnimation.png", "apng");
+
+//    movie.setScaledSize(QSize(100, 100));
+
+    QLabel label;
+    label.resize(292, 254);
+    label.setScaledContents(true);
+    QBitmap mask(label.size());
+    QPainter painterMask(&mask);
+    painterMask.setRenderHint(QPainter::Antialiasing);
+    painterMask.setRenderHint(QPainter::SmoothPixmapTransform);
+    painterMask.fillRect(0, 0, label.size().width(), label.size().height(), Qt::white);
+    painterMask.setBrush(QColor(0, 0, 0));
+    painterMask.drawRoundedRect(0, 0, label.size().width(), label.size().height(), 20, 20);
+    label.setMask(mask);
+
+    label.setMovie(&movie);
+    layout.addWidget(&label);
+    w.setLayout(&layout);
+
+    w.show();
+    movie.start();
+//    RoundedApnganimationLabel animation;
+//    animation.setApnganimationFileName("D:/work/axxstudent/Bin/Environment/images/groupRoom/openCurtainAnimation.png");
+//    animation.setApngRoundedRadius(20);
+//    layout.addWidget(&animation);
+//    w.setLayout(&layout);
+//    w.show();
+//    animation.startApnganimation();
+
+    return a.exec();
+}
---
+++ ImageReader/widget.cpp
@@ -0,0 +1,24 @@
+﻿#include "widget.h"
+#include <QImageReader>
+#include <QLabel>
+#include <QPixmap>
+
+Widget::Widget(QWidget *parent)
+    : QWidget(parent)
+{
+    QLabel* label = new QLabel(this);
+    label->setFixedSize(200, 900);
+
+    QImageReader reader("D:/timg.jpg");
+    reader.setClipRect(QRect(600, 0, 200, 900));
+    reader.setClipRect(QRect(0, 0, 200, 900));
+
+    QPixmap pixmap = QPixmap::fromImageReader(&reader);
+    label->setPixmap(pixmap);
+}
+
+Widget::~Widget()
+{
+
+}
+
