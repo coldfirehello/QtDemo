3c4b4016276683658962547ecce7265fad51cfd1
guanxinnan@aixuexi.com <guanxinnan@aixuexi.com>
2020-11-19 10:04:07 +0800

    红心

---
+++ Draw/heart.h
@@ -0,0 +1,18 @@
+﻿#ifndef HEART_H
+#define HEART_H
+
+#include <QWidget>
+
+class Heart : public QWidget
+{
+    Q_OBJECT
+
+public:
+    Heart(QWidget *parent = nullptr);
+    ~Heart();
+
+protected:
+    void paintEvent(QPaintEvent *event) override;
+
+};
+#endif // HEART_H
---
+++ Draw/main.cpp
@@ -0,0 +1,11 @@
+﻿#include "heart.h"
+
+#include <QApplication>
+
+int main(int argc, char *argv[])
+{
+    QApplication a(argc, argv);
+    Heart w;
+    w.show();
+    return a.exec();
+}
